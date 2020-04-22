4f626e0a717865e3013db63880ebb221dc38fc1d
Victor Galhardo Vela <victorgvela@hotmail.com>
2020-04-21 21:33:13 -0300

    ajustes finais

--- javascript/particles.js
+++ javascript/particles.js
@@ -1070,93 +1070,93 @@ var pJS = function(tag_id, params){
 
 
     /* detect mouse pos - on hover / click event */
-    // if(pJS.interactivity.events.onhover.enable || pJS.interactivity.events.onclick.enable){
+    if(pJS.interactivity.events.onhover.enable || pJS.interactivity.events.onclick.enable){
 
-    //   /* el on mousemove */
-    //   pJS.interactivity.el.addEventListener('mousemove', function(e){
+      /* el on mousemove */
+      pJS.interactivity.el.addEventListener('mousemove', function(e){
 
-    //     if(pJS.interactivity.el == window){
-    //       var pos_x = e.clientX,
-    //           pos_y = e.clientY;
-    //     }
-    //     else{
-    //       var pos_x = e.offsetX || e.clientX,
-    //           pos_y = e.offsetY || e.clientY;
-    //     }
-
-    //     pJS.interactivity.mouse.pos_x = pos_x;
-    //     pJS.interactivity.mouse.pos_y = pos_y;
-
-    //     if(pJS.tmp.retina){
-    //       pJS.interactivity.mouse.pos_x *= pJS.canvas.pxratio;
-    //       pJS.interactivity.mouse.pos_y *= pJS.canvas.pxratio;
-    //     }
-
-    //     pJS.interactivity.status = 'mousemove';
-
-    //   });
-
-    //   /* el on onmouseleave */
-    //   pJS.interactivity.el.addEventListener('mouseleave', function(e){
-
-    //     pJS.interactivity.mouse.pos_x = null;
-    //     pJS.interactivity.mouse.pos_y = null;
-    //     pJS.interactivity.status = 'mouseleave';
-
-    //   });
-
-    // }
-
-    /* on click event */
-    if(pJS.interactivity.events.onclick.enable){
-
-      pJS.interactivity.el.addEventListener('click', function(){
-
-        pJS.interactivity.mouse.click_pos_x = pJS.interactivity.mouse.pos_x;
-        pJS.interactivity.mouse.click_pos_y = pJS.interactivity.mouse.pos_y;
-        pJS.interactivity.mouse.click_time = new Date().getTime();
-
-        if(pJS.interactivity.events.onclick.enable){
-
-          switch(pJS.interactivity.events.onclick.mode){
-
-            case 'push':
-              if(pJS.particles.move.enable){
-                pJS.fn.modes.pushParticles(pJS.interactivity.modes.push.particles_nb, pJS.interactivity.mouse);
-              }else{
-                if(pJS.interactivity.modes.push.particles_nb == 1){
-                  pJS.fn.modes.pushParticles(pJS.interactivity.modes.push.particles_nb, pJS.interactivity.mouse);
-                }
-                else if(pJS.interactivity.modes.push.particles_nb > 1){
-                  pJS.fn.modes.pushParticles(pJS.interactivity.modes.push.particles_nb);
-                }
-              }
-            break;
-
-            case 'remove':
-              pJS.fn.modes.removeParticles(pJS.interactivity.modes.remove.particles_nb);
-            break;
-
-            case 'bubble':
-              pJS.tmp.bubble_clicking = true;
-            break;
-
-            case 'repulse':
-              pJS.tmp.repulse_clicking = true;
-              pJS.tmp.repulse_count = 0;
-              pJS.tmp.repulse_finish = false;
-              setTimeout(function(){
-                pJS.tmp.repulse_clicking = false;
-              }, pJS.interactivity.modes.repulse.duration*1000)
-            break;
-
-          }
-
+        if(pJS.interactivity.el == window){
+          var pos_x = e.clientX,
+              pos_y = e.clientY;
+        }
+        else{
+          var pos_x = e.offsetX || e.clientX,
+              pos_y = e.offsetY || e.clientY;
         }
 
+        pJS.interactivity.mouse.pos_x = pos_x;
+        pJS.interactivity.mouse.pos_y = pos_y;
+
+        if(pJS.tmp.retina){
+          pJS.interactivity.mouse.pos_x *= pJS.canvas.pxratio;
+          pJS.interactivity.mouse.pos_y *= pJS.canvas.pxratio;
+        }
+
+        pJS.interactivity.status = 'mousemove';
+
       });
-        
+
+      /* el on onmouseleave */
+      pJS.interactivity.el.addEventListener('mouseleave', function(e){
+
+        pJS.interactivity.mouse.pos_x = null;
+        pJS.interactivity.mouse.pos_y = null;
+        pJS.interactivity.status = 'mouseleave';
+
+      });
+
     }
+
+    /* on click event */
+    // if(pJS.interactivity.events.onclick.enable){
+
+    //   pJS.interactivity.el.addEventListener('click', function(){
+
+    //     pJS.interactivity.mouse.click_pos_x = pJS.interactivity.mouse.pos_x;
+    //     pJS.interactivity.mouse.click_pos_y = pJS.interactivity.mouse.pos_y;
+    //     pJS.interactivity.mouse.click_time = new Date().getTime();
+
+    //     if(pJS.interactivity.events.onclick.enable){
+
+    //       switch(pJS.interactivity.events.onclick.mode){
+
+    //         case 'push':
+    //           if(pJS.particles.move.enable){
+    //             pJS.fn.modes.pushParticles(pJS.interactivity.modes.push.particles_nb, pJS.interactivity.mouse);
+    //           }else{
+    //             if(pJS.interactivity.modes.push.particles_nb == 1){
+    //               pJS.fn.modes.pushParticles(pJS.interactivity.modes.push.particles_nb, pJS.interactivity.mouse);
+    //             }
+    //             else if(pJS.interactivity.modes.push.particles_nb > 1){
+    //               pJS.fn.modes.pushParticles(pJS.interactivity.modes.push.particles_nb);
+    //             }
+    //           }
+    //         break;
+
+    //         case 'remove':
+    //           pJS.fn.modes.removeParticles(pJS.interactivity.modes.remove.particles_nb);
+    //         break;
+
+    //         case 'bubble':
+    //           pJS.tmp.bubble_clicking = true;
+    //         break;
+
+    //         case 'repulse':
+    //           pJS.tmp.repulse_clicking = true;
+    //           pJS.tmp.repulse_count = 0;
+    //           pJS.tmp.repulse_finish = false;
+    //           setTimeout(function(){
+    //             pJS.tmp.repulse_clicking = false;
+    //           }, pJS.interactivity.modes.repulse.duration*1000)
+    //         break;
+
+    //       }
+
+    //     }
+
+    //   });
+        
+    // }
 
 
   };
